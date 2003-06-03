355c2d5e5ab8757a5fe3455b88add4cb69946742
Scott Nicol <esniper@users.sourceforge.net>
2003-06-03 00:23:29 +0000

    Port to cygwin.

--- util.c
+++ util.c
@@ -31,6 +31,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 #include <time.h>
 #include <sys/types.h>
 #if defined(unix) || defined (__unix) || defined (__MACH__)
