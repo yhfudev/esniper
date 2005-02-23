2f46e92b36d47ad1a3ce7e66ab1d3ef2833e8c04
Nils Rottgardt <nilsrott@users.sourceforge.net>
2005-02-23 09:40:16 +0000

    *** empty log message ***

--- frontends/php/setperms.sh
+++ frontends/php/setperms.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 chmod 0644 *
-chmod 0755 ./*.sh
+chmod 0755 ./esniperstart.sh
 chmod 0755 ./updateDB.php
 chmod 0755 ./reload.php
 chmod 0755 ./esniper
+echo "Do not forget to check the interpreter path in updateDB.php, reload.php and esniperstart.sh"
