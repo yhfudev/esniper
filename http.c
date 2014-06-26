36de41273e8ee49c09bab1103c5cd91885ca001d
Bodo <bomm@users.sourceforge.net>
2014-06-26 08:57:41 +0000

    fix for bug #670 using patch from Debian package to force SSLv3

--- http.c
+++ http.c
@@ -228,6 +228,12 @@ initCurlStuff(void)
 		 (curlrc = curl_easy_setopt(easyhandle, CURLOPT_PROXY, options.proxy)))
 		return initCurlStuffFailed();
 
+#if 1
+	/* enforce usage of SSLv3 to avoid GnuTLS errors */
+	if ((curlrc = curl_easy_setopt(easyhandle, CURLOPT_SSLVERSION, CURL_SSLVERSION_SSLv3)))
+		return initCurlStuffFailed();
+#endif
+
 	/* send all data to this function */
 	if ((curlrc = curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback)))
 		return initCurlStuffFailed();
